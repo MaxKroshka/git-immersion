35fe6351c7bebc00713643f268a47b3f1297544c
Max Kroshka <max.kroshka (at) gmail.com>
2015-11-21 16:55:19 -0800

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Max Kroshka (max.kroshka@gmail.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
